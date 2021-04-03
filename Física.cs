7771e1d0d70ecc4cb3702d13c6eff000bfedd351
beto-bit <76757367+beto-bit@users.noreply.github.com>
2021-04-02 22:31:36 -0600

    🎇Iniciamos el Proyecto
    Iniciamos el proyecto (y además empiezo a imprimir la interfaz de consola).

---
+++ Física.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MRUV_Solver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                // Imprimir la interfaz de consola
+                Console.WriteLine("|===========: Buenos Días :===========|\n" + 
+                                  "<><><><><><><><><><><><>><><><><><><><>\n" + 
+                                  "╔═════════════════════════════════════╗\n" +
+                                  "║   Nombre Variable ------- Índice    ║\n" +
+                                  "╠═════════════════════════════════════╣\n" +
+                                  "║   Velocidad Final --------- VF      ║\n" +
+                                  "║   Velocidad Inicial ------- VI      ║\n" +
+                                  "║   Aceleración ------------- A       ║\n" +
+                                  "║   Tiempo ------------------ T       ║\n" +
+                                  "║   Distancia --------------- X       ║\n" +
+                                  "╚═════════════════════════════════════╝\n");
+            } while (false);
+        }
+    }
+
+    class VariableSolver
+    {
+        // Constructor de Clase
+        public VariableSolver() 
+        {
+
+        }
+    }
+}
